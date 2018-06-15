d6a8015b3ec945bc54d412f27009a392b42a179d
Lola Simpson <lolalsimp@gmail.com>
2018-06-14 20:32:54 -0500

    wip

--- javascripts/messages/message_main.js
+++ javascripts/messages/message_main.js
@@ -1,9 +1,9 @@
 const messageEvents = require('./message_events');
-const messageFirebase = require('./message_firebase');
+// const messageFirebase = require('./message_firebase');
 
 const initMessageBoard = () => {
   messageEvents.addMessageEvent();
-  messageFirebase.getMessages();
+  // messageFirebase.getMessages();
 };
 
 module.exports = {
