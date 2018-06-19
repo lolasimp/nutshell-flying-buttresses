9699ddc940c2b7cf28269525cd12481bc8175647
Amanda Slayton <alslayton86@gmail.com>
2018-06-18 23:45:37 -0500

    styles- divs are now somehow seperated

--- javascripts/events/events_dom.js
+++ javascripts/events/events_dom.js
@@ -7,13 +7,13 @@ const eventsAdded = (ThingsToDoArray) => {
   // strang += `<div><span>Upcoming Events<span></div>`;
   ThingsToDoArray.forEach((item) => {
     strang += `<div class="item" data-firebase-id="${item.id}">`;
-    strang += `<h3 class="item-event">${item.event}</h3>`;
-    strang += `<h5 class="item-location">${item.location}</h5>`;
-    strang += `<h5 class="item=start">${item.startDate}</h5>`;
-    strang += `<div class="item-user"> `;
-    strang += `<button class='btn btn-default btn-message-edit editBtn hide' data-toggle="modal" data-target=""><span class='glyphicon glyphicon-pencil ignore-click'></span></button>`;
-    strang += `<button type="button" class='btn btn-default btn-message-delete deleteBtn hide'><span class='glyphicon glyphicon-trash ignore-click'></span></button>`;
-    strang += `</div>`;
+    strang +=   `<h3 class="item-event">${item.event}</h3>`;
+    strang +=   `<h5 class="item-location">${item.location}</h5>`;
+    strang +=   `<h5 class="item=start">${item.startDate}</h5>`;
+    strang +=   `<div class="item-user"> `;
+    strang +=     `<button class='btn btn-default btn-message-edit editBtn hide' data-toggle="modal" data-target=""><span class='glyphicon glyphicon-pencil ignore-click'></span></button>`;
+    strang +=     `<button type="button" class='btn btn-default btn-message-delete deleteBtn hide'><span class='glyphicon glyphicon-trash ignore-click'></span></button>`;
+    strang +=   `</div>`;
     strand += `</div>`;
     // }
   });
