e67f2ea1c2b498701096be7a4f2aada995a03ff9
Adam Wieckert <wiec1369@gmail.com>
2018-06-19 12:35:35 -0500

    Added comment in Corey's Dom to explain whats goin on

--- javascripts/articles/javascripts/dom.js
+++ javascripts/articles/javascripts/dom.js
@@ -15,6 +15,10 @@ const domStringBuilder = (articleArr, user) =>
     domString += `</div>`;
   });
 
+  // Currently the friendsArticleArry parameter has been left out of your domStringBuilder, it breaks this function if we use the delete and edit buttons.
+
+  //this is because of the parameters being passed to the domStringBuilder else where, you can I can fix this if we have time. Currently working now, but the edit and delete print out every article.
+
   // friendsArticleArr.forEach(articles =>
   // {
   //   domString += `<div class='article' id='individualArticle' data-firebase-id="${articles.id}">`;
